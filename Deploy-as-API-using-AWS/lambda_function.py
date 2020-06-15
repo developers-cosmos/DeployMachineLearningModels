34808e22dab9c2820ffd2b59545af52c731b9abc
THARUN KUMAR TALLAPALLI <43419897+tharun435@users.noreply.github.com>
2020-06-15 19:12:55 +0530

    Update

---
+++ Deploy-as-API-using-AWS/lambda_function.py
@@ -0,0 +1,28 @@
+import os
+import io
+import boto3
+import json
+import csv
+
+# grab environment variables
+ENDPOINT_NAME = os.environ['ENDPOINT_NAME']
+runtime= boto3.client('runtime.sagemaker')
+
+def lambda_handler(event, context):
+    print("Received event: " + json.dumps(event, indent=2))
+    
+    data = json.loads(json.dumps(event))
+    payload = data['data']
+    print(payload)
+    response = runtime.invoke_endpoint(EndpointName=ENDPOINT_NAME,
+                                       ContentType='text/csv',Body=payload)
+    result = json.loads(response['Body'].read().decode())
+    if(result==0):
+        result="Setosa"
+    elif(result=="1"):
+        result="Versicolor"
+    else:
+        result="Virginica"
+    return result
+        
+        
